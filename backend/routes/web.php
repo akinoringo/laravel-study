e7f60b40e47398239d970d0145874ab202856e20
akinoringo <wbraver@outlook.jp>
2021-07-17 09:40:03 +0900

    feat: laravel/uiのインストールとredirectToの修正

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 // 記事一覧
 Route::get('/', 'ArticleController@index');
+
+// 認証関連
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
